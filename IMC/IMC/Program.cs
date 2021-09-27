2b64586058f569ec5da85994623e93cf5cfc154a
Williaml42 <56610559+Williaml42@users.noreply.github.com>
2021-09-27 17:45:10 -0300

    Add files via upload

---
+++ IMC/IMC/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace IMC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite seu peso:");
+            double massa = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite sua altura");
+            double altura = double.Parse(Console.ReadLine());
+            double imc = massa / (altura * altura);
+            Console.WriteLine();
+            Console.WriteLine("Seu imc é:"+ imc);
+        }
+    }
+}
